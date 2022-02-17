e0307212b2ba9de13878242c76bf150d306bb82a
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2022-02-16 21:56:55 -0500

    Restructure faq accordion

--- src/components/FaqAccordion.vue
+++ src/components/FaqAccordion.vue
@@ -1,76 +1,61 @@
 <template>
   <div class="faq-container">
-    <div class="accordion">
-      <div class="accordion-item">
+    <div class="accordion" v-for="faq in faqData" :key="faq">
+      
+      <div class="accordion-item" @click="handleClick(faq)">
         <button class="accordion-question">
-          A digital agency is a business
+          {{ faq.question}}
           <img class="arrow" src="../assets/images/arrow.png" alt="expand" />
         </button>
-        <div class="accordion-collapse">
+        <div class="accordion-collapse collapse" :class="[faq.isOpen ? collapse : '']">
           <div class="accordion-content">
-            Digital marketing efforts, instead of handling in-house. They can
-            provide your business with a variety of digital solutions to promote
-            your product or service online and help you hit your marketing goals
-            and grow your business.
+            {{ faq.answer }}
           </div>
         </div>
       </div>
-
-      <div class="accordion-item">
-        <button class="accordion-question">
-          Hire to outsource your digital
-          <img class="arrow" src="../assets/images/arrow.png" alt="expand" />
-        </button>
-        <div class="accordion-collapse collapse">
-          <div class="accordion-content">
-            No more than 2GB. All files in your account must fit your allotted
-            storage space.
-          </div>
-        </div>
-      </div>
-
-      <div class="accordion-item">
-        <button class="accordion-question">
-          Marketing efforts
-          <img class="arrow" src="../assets/images/arrow.png" alt="expand" />
-        </button>
-        <div class="accordion-collapse collapse">
-          <div class="accordion-content">
-            Click “Forgot password” from the login page or “Change password”
-            from your profile page. A reset link will be emailed to you.
-          </div>
-        </div>
-      </div>
-      <!-- 
-					<div class="accordion-item">
-						<button class="accordion-question">
-							Can I cancel my subscription?
-						</button>
-						<div class="accordion-collapse collapse">
-							<div class="accordion-content">
-								Yes! Send us a message and we'll process your request no
-								questions asked.
-							</div>
-						</div>
-					</div>
-
-					<div class="accordion-item">
-						<button class="accordion-question">
-							Do you provide additional support?
-						</button>
-						<div class="accordion-collapse collapse">
-							<div class="accordion-content">
-								Chat and email support is available 24/7. Phone lines are open
-								during normal business hours.
-							</div>
-						</div>
-					</div> -->
+      
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { watchEffect, ref } from 'vue'
+export default {
+  name: "Faq",
+  setup() {
+
+    const faqData = ref([
+      {
+        id: 1,
+        question: "A digital agency is a business",
+        answer:
+          "Digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you hit your marketing goals and grow your business.",
+        isOpen: false,
+      },
+      {
+        id: 2,
+        question: "Hire to outsource your digital",
+        answer:
+          "Digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you hit your marketing goals and grow your business.",
+        isOpen: false,
+      },
+      {
+        id: 3,
+        question: "Marketing efforts",
+        answer:
+          "Digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you hit your marketing goals and grow your business.",
+        isOpen: false,
+      },
+    ]);
+
+    const handleClick = (data) => {
+     data.isOpen = !data.isOpen;
+      console.log(data);
+    };
+
+    return { handleClick, faqData };
+  }
+};
 </script>
 
 <style>
@@ -89,10 +74,12 @@ export default {};
 
 .accordion-item {
   border: 1px solid #f3d1bf;
+  cursor: pointer;
 }
 
 .arrow {
   width: 2rem;
+  content: url("../assets/images/arrow.png");
 }
 
 .accordion-question {
@@ -134,5 +121,35 @@ export default {};
 
 .accordion__collapse.open {
   display: block;
+}
+
+.vcp__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 6.5rem;
+  font-size: 24px;
+  font-weight: 700;
+  padding: 0 2rem 0 2rem;
+}
+.vcp__header-icon[data-v-64321428] {
+  display: flex;
+  align-items: center;
+  transition: transform 0.3s cubic-bezier(0.5, 0.25, 0, 1);
+}
+.vcp--expanded .vcp__header-icon[data-v-64321428] {
+  transform-origin: center;
+  transform: rotate(180deg);
+}
+
+.vcp__header-icon > span[data-v-64321428] {
+  display: flex;
+  height: 24px;
+  width: 24px;
+}
+.vcp__header-icon[data-v-64321428] svg {
+  height: 100%;
+  width: 100%;
 }
 </style>
