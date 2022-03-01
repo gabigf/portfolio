c2e60c282179ddf6ed07736d8685010689ba0422
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2022-03-01 02:38:45 -0500

    Add resume markup

--- src/components/TheResume.vue
+++ src/components/TheResume.vue
@@ -1,16 +1,120 @@
 <template>
   <section class="Resume">
-    
+    <header class="resume-header">
+      <span class="name">resume</span>
+      <h2 class="resume-heading">Professional Review</h2>
+    </header>
 
+    <section class="resume-left">
+      <div class="work-experience">
+        <h3>Work Experience</h3>
+        <article>
+          <p>Crew Cafe & Collective - Barista Manager</p>
+          <p>Dec 2018 - Jun 2020</p>
+        </article>
+        <article>
+          <p>Saint-Henri Micro-Torréfacteur - Wholesale Representative</p>
+          <p>Jan 2018 - Dec 2020</p>
+        </article>
+      </div>
+
+      <div class="tech-skills">
+        <h3>Technical Skills</h3>
+        <div class="left-skills">
+          <p>Javascript</p>
+          <p>VueJS</p>
+          <p>ReactJS</p>
+          <p>jQuery</p>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>SASS/SCSS</p>
+          <p>Firebase</p>
+        </div>
+        <div class="right-skills">
+          <p>Ruby on Rails</p>
+          <p>SQL</p>
+          <p>PostgreSQL</p>
+          <p>PHP</p>
+          <p>MongoDB</p>
+          <p>Express</p>
+          <p>NodeJS</p>
+          <p>Mocha</p>
+        </div>
+      </div>
+
+      <div class="tools">
+        <h3>Tools</h3>
+        <div class="left-skills">
+          <p>Git</p>
+          <p>Github</p>
+          <p>CLI</p>
+          <p>npm</p>
+        </div>
+        <div class="right-skills">
+          <p>Figma</p>
+          <p>Bootstrap</p>
+          <p>Storybook</p>
+          <p>Cypress</p>
+        </div>
+      </div>
+
+      <div class="langs">
+        <p>English</p>
+        <p>French</p>
+        <p>Portuguese</p>
+      </div>
+
+    </section>
+
+    <section class="resume-right">
+      <div class="education">
+        <h3>Education</h3>
+        <article>
+          <p>Lighthouse Labs</p>
+          <p>Oct 2021 - Jan 2021</p>
+          <p>Full Stack Web Development Bootcamp</p>
+        </article>
+        <article>
+          <p>Lighthouse Labs</p>
+          <p>Oct 2021 - Jan 2021</p>
+          <p>Full Stack Web Development Bootcamp</p>
+        </article>
+      </div>
+
+      <div class="online-courses">
+        <h3>Online Courses</h3>
+        <article>
+          <p>Lighthouse Labs</p>
+          <p>Oct 2021 - Jan 2021</p>
+          <p>Full Stack Web Development Bootcamp</p>
+        </article>
+        <article>
+          <p>Lighthouse Labs</p>
+          <p>Oct 2021 - Jan 2021</p>
+          <p>Full Stack Web Development Bootcamp</p>
+        </article>
+      </div>
+
+      <div class="Awards">
+        <h3>Awards</h3>
+        <article>
+          <p>SCA</p>
+          <p>Spring</p>
+          <p>Full Stack Web Development Bootcamp</p>
+        </article>
+        <article>
+          <p>SCA</p>
+          <p>Spring</p>
+          <p>Full Stack Web Development Bootcamp</p>
+        </article>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
