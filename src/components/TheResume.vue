fcb6db7375dc88758fd8420afe9625c17fd09d63
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2022-03-02 01:13:13 -0500

    Finish resume styling

--- src/components/TheResume.vue
+++ src/components/TheResume.vue
@@ -11,54 +11,62 @@
           <p class="title-line"></p>
           <h3 class="resume-title">Work Experience</h3>
           <article>
-            <p>Crew Cafe & Collective - Barista Manager</p>
-            <p>Dec 2018 - Jun 2020</p>
+            <p class="category-title">
+              Crew Cafe & Collective - Barista Manager
+            </p>
+            <p class="category-details">Dec 2018 - Jun 2020</p>
           </article>
           <article>
-            <p>Saint-Henri Micro-Torréfacteur - Wholesale Representative</p>
-            <p>Jan 2018 - Dec 2020</p>
+            <p class="category-title">
+              Saint-Henri Micro-Torréfacteur - Wholesale Representative
+            </p>
+            <p class="category-details">Jan 2018 - Dec 2020</p>
           </article>
         </div>
 
         <div class="tech-skills resume-component">
           <p class="title-line"></p>
           <h3 class="resume-title">Technical Skills</h3>
-          <div class="left-skills">
-            <p>Javascript</p>
-            <p>VueJS</p>
-            <p>ReactJS</p>
-            <p>jQuery</p>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>SASS/SCSS</p>
-            <p>Firebase</p>
-          </div>
-          <div class="right-skills">
-            <p>Ruby on Rails</p>
-            <p>SQL</p>
-            <p>PostgreSQL</p>
-            <p>PHP</p>
-            <p>MongoDB</p>
-            <p>Express</p>
-            <p>NodeJS</p>
-            <p>Mocha</p>
+          <div class="category-details-container">
+            <div class="left-skills">
+              <p>Javascript</p>
+              <p>VueJS</p>
+              <p>ReactJS</p>
+              <p>jQuery</p>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>SASS/SCSS</p>
+              <p>Firebase</p>
+            </div>
+            <div class="right-skills">
+              <p>Ruby on Rails</p>
+              <p>SQL</p>
+              <p>PostgreSQL</p>
+              <p>PHP</p>
+              <p>MongoDB</p>
+              <p>Express</p>
+              <p>NodeJS</p>
+              <p>Mocha</p>
+            </div>
           </div>
         </div>
 
         <div class="tools resume-component">
           <p class="title-line"></p>
           <h3 class="resume-title">Tools</h3>
-          <div class="left-skills">
-            <p>Git</p>
-            <p>Github</p>
-            <p>CLI</p>
-            <p>npm</p>
-          </div>
-          <div class="right-skills">
-            <p>Figma</p>
-            <p>Bootstrap</p>
-            <p>Storybook</p>
-            <p>Cypress</p>
+          <div class="category-details-container">
+            <div class="left-skills">
+              <p>Git</p>
+              <p>Github</p>
+              <p>CLI</p>
+              <p>npm</p>
+            </div>
+            <div class="right-skills">
+              <p>Figma</p>
+              <p>Bootstrap</p>
+              <p>Storybook</p>
+              <p>Cypress</p>
+            </div>
           </div>
         </div>
 
@@ -76,13 +84,13 @@
           <p class="title-line"></p>
           <h3 class="resume-title">Education</h3>
           <article>
-            <p>Lighthouse Labs</p>
-            <p>Oct 2021 - Jan 2021</p>
+            <p class="category-title">Lighthouse Labs</p>
+            <p class="category-details">Oct 2021 - Jan 2021</p>
             <p>Full Stack Web Development Bootcamp</p>
           </article>
           <article>
-            <p>Lighthouse Labs</p>
-            <p>Oct 2021 - Jan 2021</p>
+            <p class="category-title">Lighthouse Labs</p>
+            <p class="category-details">Oct 2021 - Jan 2021</p>
             <p>Full Stack Web Development Bootcamp</p>
           </article>
         </div>
@@ -91,13 +99,18 @@
           <p class="title-line"></p>
           <h3 class="resume-title">Online Courses</h3>
           <article>
-            <p>Lighthouse Labs</p>
-            <p>Oct 2021 - Jan 2021</p>
+            <p class="category-title">Lighthouse Labs</p>
+            <p class="category-details">Oct 2021 - Jan 2021</p>
             <p>Full Stack Web Development Bootcamp</p>
           </article>
           <article>
-            <p>Lighthouse Labs</p>
-            <p>Oct 2021 - Jan 2021</p>
+            <p class="category-title">Lighthouse Labs</p>
+            <p class="category-details">Oct 2021 - Jan 2021</p>
+            <p>Full Stack Web Development Bootcamp</p>
+          </article>
+          <article>
+            <p class="category-title">Lighthouse Labs</p>
+            <p class="category-details">Oct 2021 - Jan 2021</p>
             <p>Full Stack Web Development Bootcamp</p>
           </article>
         </div>
@@ -106,13 +119,13 @@
           <p class="title-line"></p>
           <h3 class="resume-title">Awards</h3>
           <article>
-            <p>SCA</p>
-            <p>Spring</p>
+            <p class="category-title">SCA</p>
+            <p class="category-details">Spring</p>
             <p>Full Stack Web Development Bootcamp</p>
           </article>
           <article>
-            <p>SCA</p>
-            <p>Spring</p>
+            <p class="category-title">SCA</p>
+            <p class="category-details">Spring</p>
             <p>Full Stack Web Development Bootcamp</p>
           </article>
         </div>
@@ -143,8 +156,14 @@ export default {};
   display: flex;
 }
 
+.resume-left {
+  margin-right: 6.5rem;
+}
+
 .resume-component {
   margin-top: 5.5rem;
+  font-size: 1.25rem;
+  line-height: 2rem;
 }
 
 .title-line {
@@ -157,5 +176,27 @@ export default {};
 
 .resume-title {
   font-size: 2rem;
+  margin-bottom: 2rem;
+}
+
+.category-title {
+  font-weight: 600;
+}
+
+.category-details {
+  color: var(--light-font);
+}
+
+.resume-component article:first-of-type, .resume-component article:nth-of-type(2)  {
+  margin-bottom: 2rem;
+}
+
+.category-details-container {
+  display: flex;
+}
+
+.left-skills {
+  margin-right: 7.5rem;
+  width: 114px;
 }
 </style>
